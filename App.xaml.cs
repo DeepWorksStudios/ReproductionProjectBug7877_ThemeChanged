9f951de899184801184faa1e267690c18de3dcd8
DeepWorksStudios <deepworks.studios@gmail.com>
2022-06-09 15:20:05 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ReproductionProjectBug_7877;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Platforms/Android/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using System.Diagnostics;
+
+namespace ReproductionProjectBug_7877;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+    protected override void OnNightModeChanged(int mode)
+    {
+        base.OnNightModeChanged(mode);
+        System.Diagnostics.Debug.WriteLine("OnNightMode Changed Call");
+    }
+}
