787846c8a4a63108c58a19837981f2971eb2b5e1
DeepWorksStudios <deepworks.studios@gmail.com>
2022-06-10 09:34:15 +0200

    added Workaround

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,10 +2,26 @@
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+   
+    public App()
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        MainPage = new AppShell();
+
+#if ANDROID
+        Application.Current.RequestedThemeChanged += Android_ThemeChanged;
+#endif
+
+    }
+
+    private void Android_ThemeChanged(object sender, AppThemeChangedEventArgs args)
+    {
+        if (MainActivity.AndroidInstance == null) return;
+
+        bool darkState = Application.Current.RequestedTheme == AppTheme.Dark ? true : false;
+        MainActivity.AndroidInstance.OnNightModeChangedManual(darkState);
+    }
+
+
 }
--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -8,9 +8,27 @@ namespace ReproductionProjectBug_7877;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    #region Singleton
+    private static MainActivity _androidInstance;
+    public static MainActivity AndroidInstance { get { return _androidInstance; } }
+    #endregion
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        _androidInstance = new MainActivity();
+    }
+  
+    //Not Working Nativ Call
     protected override void OnNightModeChanged(int mode)
     {
         base.OnNightModeChanged(mode);
         System.Diagnostics.Debug.WriteLine("OnNightMode Changed Call");
+    }
+    //Manual Call
+    public void OnNightModeChangedManual(bool darkMode)
+    {
+        
+        System.Diagnostics.Debug.WriteLine("OnNightModeChanged Manual Call");
     }
 }
